2e0786739137bf22866db30b77a9e4d881b6783a
Haider Ali <rmha62@gmail.com>
2025-07-25 05:18:45 +0500

    Add dynamic API function generation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+import re
 import json
 import hmac
 import hashlib
 import sqlite3
+from jinja2 import Template
 from dotenv import dotenv_values
 from rich.console import Console
 
@@ -90,3 +92,206 @@ console.print("content types:", content_types)
 console.print("token:", key)
 
 conn.close()
+
+# \"\"\"
+# {{ docstring }}
+# \"\"\"
+
+function_template = Template(
+    f"""
+
+{
+    "def {{ name }}"
+    "("
+    "{% for arg in args %}"
+    "{{ arg.name }}{% if arg.suffix %}{{ arg.suffix }}{% endif %}: {{ arg.type }}"
+    "{% if is_first_optional or not loop.first %}"
+    "{% if arg.default %} = {{ arg.default }}{% else %} = None{% endif %}"
+    "{% endif %}"
+    "{% if not loop.last %}, {% endif %}"
+    "{% endfor %}"
+    ")"
+    "{% if return_type %} -> {{ return_type }}{% endif %}:"
+    "\n    {{ body|indent(4) }}"
+}
+
+"""
+)
+
+file = f"""
+import json
+import requests
+from typing import Literal
+from rich.console import Console
+
+console = Console()
+
+headers = {{
+    "Authorization": "Bearer {key}",
+    "Content-Type": "application/json"
+}}
+"""
+
+functions = ""
+function_names = []
+
+for content_type in content_types.values():
+    for action in content_type["actions"]:
+        singular_name = content_type["info"]["singularName"]
+        plural_name = content_type["info"]["pluralName"]
+
+        simple_action = {
+            "find": "find_all",
+            "findOne": "find",
+            "create": "create",
+            "update": "update",
+            "delete": "delete",
+        }[action]
+
+        args = []
+
+        if simple_action in ["find", "update", "delete"]:
+            args.append(
+                {
+                    "name": singular_name,
+                    "type": "str",
+                    "suffix": "_id",
+                    "is_not_payload": True,
+                }
+            )
+
+        if simple_action in ["create", "update"]:
+            for field in content_type["attributes"]:
+                if content_type["attributes"][field]["type"] == "integer":
+                    args.append({"name": field, "type": "int | None"})
+                elif content_type["attributes"][field]["type"] == "string":
+                    args.append({"name": field, "type": "str | None"})
+                elif content_type["attributes"][field]["type"] == "boolean":
+                    args.append({"name": field, "type": "bool | None"})
+                elif content_type["attributes"][field]["type"] == "enumeration":
+                    values = content_type["attributes"][field]["enum"]
+                    args.append(
+                        {"name": field, "type": f"Literal{json.dumps(values)} | None"}
+                    )
+                elif content_type["attributes"][field]["type"] == "relation":
+                    single = content_type["attributes"][field]["relation"].endswith(
+                        "One"
+                    )
+                    id_or_ids = "_id" if single else "_ids"
+                    args.append(
+                        {
+                            "name": field,
+                            "type": f"{'str' if single else 'list[str]'} | None",
+                            "suffix": id_or_ids,
+                            "is_relation": True,
+                            "is_single": single,
+                        }
+                    )
+                    if simple_action == "update":
+                        args.append(
+                            {
+                                "name": field,
+                                "type": 'Literal["add", "remove"] | None',
+                                "suffix": f"{id_or_ids}_action",
+                                "is_action": True,
+                            }
+                        )
+                else:
+                    args.append({"name": field, "type": "str | None"})
+
+        if action == "find":
+            body = f"""
+                url = f"{project_origin}/api/{plural_name}"
+                return requests.request("GET", url, headers=headers).json()
+            """
+
+        elif action == "findOne":
+            body = f"""
+                url = f"{project_origin}/api/{plural_name}/{{{singular_name}_id}}"
+                return requests.request("GET", url, headers=headers).json()
+            """
+
+        elif action == "create":
+            data = ", ".join(
+                [
+                    (
+                        f'"{arg["name"]}": None if {var_name} is None else {{ '
+                        f'"connect": '
+                        f'{"[" if is_single else ""}{var_name}{"]" if is_single else ""} }}'
+                        if arg.get("is_relation", False)
+                        else f'"{arg["name"]}": {var_name}'
+                    )
+                    for arg in args
+                    if not arg.get("is_action", False)
+                    and not arg.get("is_not_payload", False)
+                    for var_name, is_single in [
+                        (
+                            f'{arg["name"]}{arg.get("suffix", "")}',
+                            arg.get("is_single", False),
+                        )
+                    ]
+                ]
+            )
+            body = f"""
+                data = {{{data}}}
+                data = {{k: v for k, v in data.items() if v is not None}}
+                url = f"{project_origin}/api/{plural_name}"
+                return requests.request("POST", url, headers=headers, data=json.dumps({{"data": data}})).json()
+            """
+
+        elif action == "update":
+            data = ", ".join(
+                [
+                    (
+                        f'"{arg["name"]}": None if {var_name}_action is None or {var_name} is None else {{ '
+                        f'("connect" if {var_name}_action == "add" else "disconnect"): '
+                        f'{"[" if is_single else ""}{var_name}{"]" if is_single else ""} }}'
+                        if arg.get("is_relation", False)
+                        else f'"{arg["name"]}": {var_name}'
+                    )
+                    for arg in args
+                    if not arg.get("is_action", False)
+                    and not arg.get("is_not_payload", False)
+                    for var_name, is_single in [
+                        (
+                            f'{arg["name"]}{arg.get("suffix", "")}',
+                            arg.get("is_single", False),
+                        )
+                    ]
+                ]
+            )
+            body = f"""
+                data = {{{data}}}
+                data = {{k: v for k, v in data.items() if v is not None}}
+                url = f"{project_origin}/api/{plural_name}/{{{singular_name}_id}}"
+                return requests.request("PUT", url, headers=headers, data=json.dumps({{"data": data}})).json()
+            """
+
+        elif action == "delete":
+            body = f"""
+                url = f"{project_origin}/api/{plural_name}/{{{singular_name}_id}}"
+                return requests.request("DELETE", url, headers=headers).json()
+            """
+
+        else:
+            body = f"""
+                return {{error: "No implementation for this action yet"}}
+            """
+
+        function_name = f"{simple_action}_{plural_name if simple_action.endswith("all") else singular_name}"
+        function_names.append(function_name)
+
+        functions += function_template.render(
+            **{
+                "is_first_optional": simple_action in ["create", "find_all"],
+                "name": function_name,
+                "args": args,
+                "returns": "dict",
+                "body": re.sub(r"^ {16}", "", body.strip(), flags=re.MULTILINE),
+            }
+        )
+
+with open("generated_functions.py", "w") as f:
+    f.write(file + functions + f"\ntools = [{", ".join(function_names)}]\n")
+
+# console.print(find_service("tvyt9r24jtt9qwk55hrg93yf"))
